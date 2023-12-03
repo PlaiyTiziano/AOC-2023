179d8739ea201720cd2dd20e8bf45b3930dd6828
Plaiy Tiziano <tiziano.plaiy@gmail.com>
2023-12-03 18:20:09 +0100

    improve ./start_day.sh some more

--- start_day.sh
+++ start_day.sh
@@ -1,6 +1,11 @@
-mkdir "$HOME/personal/AOC-2023/day-$1"
+mkdir -p "$HOME/personal/AOC-2023/day-$1/chapters"
 
 touch "$HOME/personal/AOC-2023/day-$1/main.go"
+
+touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter1.go"
+touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter1_test.go"
+touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter2.go"
+touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter2_test.go"
 
 # Fetch the input
 curl -H "Cookie: session=`cat $HOME/personal/AOC-2023/cookie.txt`" \
