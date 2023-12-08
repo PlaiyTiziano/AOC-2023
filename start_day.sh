e788e39602625fe9c2b3de0346a9be1ac66e8d38
Plaiy Tiziano <tiziano.plaiy@gmail.com>
2023-12-08 23:20:44 +0100

    ./start_day now populates the files as well

--- start_day.sh
+++ start_day.sh
@@ -1,13 +1,143 @@
-mkdir -p "$HOME/personal/AOC-2023/day-$1/chapters"
+#! /bin/sh
+ROOT="$HOME/personal/AOC-2023/day-$1"
 
-touch "$HOME/personal/AOC-2023/day-$1/main.go"
+mkdir -p "$ROOT/chapters"
 
-touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter1.go"
-touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter1_test.go"
-touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter2.go"
-touch "$HOME/personal/AOC-2023/day-$1/chapters/chapter2_test.go"
+touch "$ROOT/main.go"
+
+echo "package main
+
+import (
+	\"fmt\"
+
+	\"github.com/PlaiyTiziano/AOC-2023/day-$1/chapters\"
+	\"github.com/PlaiyTiziano/AOC-2023/utils\"
+)
+
+func main() {
+	fmt.Println(\"Day $1:\")
+
+	// inputFilepath := utils.BuildInputFilepath($1)
+	// content := utils.ReadFile(inputFilepath)
+
+	value := chapters.Chapter1()
+	fmt.Println(\"- Chapter 1:\", value)
+
+	value = chapters.Chapter2()
+	fmt.Println(\"- Chapter 2:\", value)
+}" >> "$ROOT/main.go"
+
+touch "$ROOT/chapters/chapter1.go"
+
+echo "package chapters
+
+func Chapter1() int {
+	return -1
+}" >> "$ROOT/chapters/chapter1.go"
+
+touch "$ROOT/chapters/chapter1_test.go"
+
+echo "package chapters
+
+import (
+	\"fmt\"
+	\"os\"
+	\"testing\"
+
+	\"github.com/PlaiyTiziano/AOC-2023/utils\"
+)
+
+func TestChapter1WithSampleInput(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf(\"Could not get current working directory\")
+	}
+
+	// inputFilepath := fmt.Sprintf(\"%s/../sample_input_chapter_1.txt\", wd)
+	// data := utils.ReadFile(inputFilepath)
+
+	value := Chapter1()
+	expectedValue := 35
+
+	if value != expectedValue {
+		t.Fatalf(\"chapter1(games) = %d, expected %d\", value, expectedValue)
+	}
+}
+
+// func TestChapter1AfterCompletingIt(t *testing.T) {
+	// wd, err := os.Getwd()
+	// if err != nil {
+		// t.Fatalf(\"Could not get current working directory\")
+	// }
+	// }
+	// 
+	// inputFilepath := fmt.Sprintf(\"%s/../input.txt\", wd)
+	// data := utils.ReadFile(inputFilepath)
+	//
+	// value := Chapter1(data)
+	// expectedValue := 322500873
+	//
+	// if value != expectedValue {
+		// t.Fatalf(\"chapter1(games) = %d, expected %d\", value, expectedValue)
+	// }
+// }" >> "$ROOT/chapters/chapter1_test.go"
+
+touch "$ROOT/chapters/chapter2.go"
+
+echo "package chapters
+
+func Chapter2() int {
+	return -1
+}" >> "$ROOT/chapters/chapter2.go"
+
+touch "$ROOT/chapters/chapter2_test.go"
+
+echo "package chapters
+
+import (
+	\"fmt\"
+	\"os\"
+	\"testing\"
+
+	\"github.com/PlaiyTiziano/AOC-2023/utils\"
+)
+
+func TestChapter2WithSampleInput(t *testing.T) {
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf(\"Could not get current working directory\")
+	}
+
+	// inputFilepath := fmt.Sprintf(\"%s/../sample_input_chapter_1.txt\", wd)
+	// data := utils.ReadFile(inputFilepath)
+
+	value := Chapter2()
+	expectedValue := 35
+
+	if value != expectedValue {
+		t.Fatalf(\"chapter2(games) = %d, expected %d\", value, expectedValue)
+	}
+}
+
+// func TestChapter2AfterCompletingIt(t *testing.T) {
+	// wd, err := os.Getwd()
+	// if err != nil {
+		// t.Fatalf(\"Could not get current working directory\")
+	// }
+	// }
+	// 
+	// inputFilepath := fmt.Sprintf(\"%s/../input.txt\", wd)
+	// data := utils.ReadFile(inputFilepath)
+	//
+	// value := Chapter2(data)
+	// expectedValue := 322500873
+	//
+	// if value != expectedValue {
+		// t.Fatalf(\"chapter2(games) = %d, expected %d\", value, expectedValue)
+	// }
+// }" >> "$ROOT/chapters/chapter2_test.go"
 
 # Fetch the input
 curl -H "Cookie: session=`cat $HOME/personal/AOC-2023/cookie.txt`" \
 	"https://adventofcode.com/2023/day/$1/input" \
-	-o "$HOME/personal/AOC-2023/day-$1/input.txt"
+	-o "$ROOT/input.txt"
